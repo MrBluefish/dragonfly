958ac019174cecb67454c7c1ef70f1308b341f56
Gino <115049934+MrBluefish@users.noreply.github.com>
2023-06-20 17:49:25 -0400

    Update dragonfly.jsx
    responsiveness

--- dragonfly.jsx
+++ dragonfly.jsx
@@ -3,5 +3,16 @@ splashScreen.addEventListener('click',()=>{
     splashScreen.style.opacity = 0;
     setTimeout(()=>{
         splashScreen.classList.add('hidden')
-    },1000)
+    },610)
     })
+
+    var header = document.querySelector('header');
+var nav = document.querySelector('nav');
+
+if (window.innerWidth < 768) {
+  header.classList.add('small-screen');
+  nav.classList.add('small-screen');
+} else {
+  header.classList.remove('small-screen');
+  nav.classList.remove('small-screen');
+}
